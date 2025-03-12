11bb1e70ce39276b28abc2cd01ce91062b295e1d
lead0lead <159436295+lead0lead@users.noreply.github.com>
2025-03-12 21:53:47 +0100

    Add initial threejs setup

--- _site/scripts/index.js
+++ _site/scripts/index.js
@@ -1,4 +1,8 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 const cursor_blob = document.getElementById("cursor_blob");
+const canvas = document.querySelector('#bg');
 
 document.body.onpointermove = event => {
     const {clientX, clientY} = event;
@@ -7,4 +11,49 @@ document.body.onpointermove = event => {
         left: `${clientX}px`,
         top: `${clientY}px`
     }, {duration: 3000, fill: "forwards" });
+}
+
+if (canvas) {
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100);
+
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector('#bg'),
+    });
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.position.setZ(30);
+
+    const geometry = new THREE.TorusGeometry( 10, 3, 16, 100);
+    const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 });
+    const torus = new THREE.Mesh( geometry, material );
+
+    scene.add(torus)
+
+    const pointLight = new THREE.PointLight(0xffffff, 10000);
+    pointLight.position.set(20,20,20);
+    scene.add(pointLight)
+
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(ambientLight);
+
+    const lightHelper = new THREE.PointLightHelper(pointLight);
+    const gridHelper = new THREE.GridHelper(200, 50);
+    scene.add(lightHelper, gridHelper)
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    function animate(){
+        requestAnimationFrame( animate );
+        torus.rotation.x += 0.01;
+        torus.rotation.y += 0.005;
+        torus.rotation.z += 0.01;
+
+        controls.update();
+
+        renderer.render( scene, camera );
+    }
+
+    animate()
 }
